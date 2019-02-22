7d2a79d4c2dbcbf402c44424fedb48750fe60255
Andrew Scobie <ascobie@microsoft.com>
2019-02-22 14:52:20 +1300

    update script

--- setup-blender-pool.sh
+++ setup-blender-pool.sh
@@ -1,17 +1,8 @@
 #!/bin/bash
 
-echo '# installing libraries for Blender'
-echo '# Running apt-get update ...'
-sudo apt-get update
-
-echo '# Installing libglu1 ...'
-sudo apt-get install libglu1
-
-echo '# Installing libxi6 ...'
-sudo apt-get install libxi6
-
-echo '# Installing libxrender1 ...'
-sudo apt-get install libxrender1
+echo '# installing required libraries for Blender'
+sudo apt update && sudo apt -y install libglu1 libxi6 libxrender1
 
 echo "## DONE ##"
 exit $?
+
